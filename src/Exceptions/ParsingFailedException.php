2bfdb6927ae442e6a90011afc495d4b33a39668a
Chad Janicek <chad@zactax.com>
2026-02-25 12:20:04 -0600

    upgrade laravel, remove ignition contracts

--- src/Exceptions/ParsingFailedException.php
+++ src/Exceptions/ParsingFailedException.php
@@ -2,19 +2,7 @@
 
 namespace TeamZac\LaraPie\Exceptions;
 
-use Facade\IgnitionContracts\Solution;
-use Facade\IgnitionContracts\BaseSolution;
-use Facade\IgnitionContracts\ProvidesSolution;
-
-class ParsingFailedException extends \RuntimeException implements ProvidesSolution 
+class ParsingFailedException extends \RuntimeException
 {
-    /** @return \Facade\IgnitionContracts\Solution[] */
-    public function getSolution(): Solution
-    {
-        return BaseSolution::create('Check the URL for your RSS feed')
-            ->setSolutionDescription('SimplePie was unable to parse the URL you provided. You might want to double check that it is a valid RSS/Atom feed.')
-            ->setDocumentationLinks([
-                'SimplePie Docs' => 'http://simplepie.org/api/source-class-SimplePie.html#691',
-            ]);
-    }
+    
 }
